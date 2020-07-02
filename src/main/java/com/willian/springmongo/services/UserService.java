f1653ac90787e0149ddce49584c22bfcbc1f0e7e
Will <willianwed98@gmail.com>
2020-07-02 16:12:24 -0300

    Connecting with mongoDB by repository and service

---
+++ src/main/java/com/willian/springmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.willian.springmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.willian.springmongo.domain.User;
+import com.willian.springmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+}
