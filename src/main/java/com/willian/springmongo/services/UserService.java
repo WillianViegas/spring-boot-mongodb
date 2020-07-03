a4b77f3fd031bec76a703b779daf97bb69ca231c
Will <willianwed98@gmail.com>
2020-07-03 11:04:31 -0300

    findById method and exceptions(ObjectNotFoundException, StandardError, ResourceExceptionHandler)

--- src/main/java/com/willian/springmongo/services/UserService.java
+++ src/main/java/com/willian/springmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.willian.springmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.willian.springmongo.domain.User;
 import com.willian.springmongo.repository.UserRepository;
+import com.willian.springmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException(id));
 	}
 }
---
+++ src/main/java/com/willian/springmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.willian.springmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String id) {
+		super("Object not found! Id: " + id);
+	}
+}
