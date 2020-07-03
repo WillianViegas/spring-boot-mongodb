40c2576b65883345b2557acfb41e0694edbb4b80
Will <willianwed98@gmail.com>
2020-07-03 14:44:25 -0300

    Endpoint posts of User

--- src/main/java/com/willian/springmongo/resources/UserResource.java
+++ src/main/java/com/willian/springmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.willian.springmongo.domain.Post;
 import com.willian.springmongo.domain.User;
 import com.willian.springmongo.dto.UserDTO;
 import com.willian.springmongo.services.UserService;
@@ -65,5 +66,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	//@GetMapping(value = "/{id}/posts"
+	@RequestMapping(value= "/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
