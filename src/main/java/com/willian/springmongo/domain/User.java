4646a21c197769b5e7edd4fbfdca89f3e6492d41
Will <willianwed98@gmail.com>
2020-07-03 14:35:14 -0300

    Referencing posts of the user

--- src/main/java/com/willian/springmongo/domain/User.java
+++ src/main/java/com/willian/springmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.willian.springmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -13,6 +16,11 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	//lazy = true retorna apenas os atributos basicos do usuario
+	//para retornar os posts é necessário acessa-los
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 	}
@@ -46,6 +54,14 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
