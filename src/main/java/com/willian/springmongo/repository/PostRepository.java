75e060a7a1b01547e54e4adcb903e9997e2a5acc
Will <willianwed98@gmail.com>
2020-07-05 16:26:02 -0300

    @Query search

--- src/main/java/com/willian/springmongo/repository/PostRepository.java
+++ src/main/java/com/willian/springmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.willian.springmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.willian.springmongo.domain.Post;
@@ -10,5 +11,12 @@ import com.willian.springmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	//Exemplo de @Query:  <field>: { $regex: /pattern/, $options: '<options>' } }
+	//Link: https://docs.mongodb.com/manual/reference/operator/query/regex/
+	@Query("{ 'title': {$regex: ?0, $options: 'i'} }")
+	List<Post> searchByTitle(String text);
+	
+	//O spring data consegue configurar tipos de pesquisas no mongo baseado em alguns nomes 
+	//de metodos. Para consulta posterior "Query methods"
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
