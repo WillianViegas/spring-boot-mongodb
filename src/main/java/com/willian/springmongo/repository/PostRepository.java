3eeba62834aa9e5eacf3162ee6d79b6fa9580dcd
Will <willianwed98@gmail.com>
2020-07-05 21:06:06 -0300

    fullSearch method

--- src/main/java/com/willian/springmongo/repository/PostRepository.java
+++ src/main/java/com/willian/springmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.willian.springmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -19,4 +20,7 @@ public interface PostRepository extends MongoRepository<Post, String>{
 	//O spring data consegue configurar tipos de pesquisas no mongo baseado em alguns nomes 
 	//de metodos. Para consulta posterior "Query methods"
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ {date: {$gte: ?1 } }, {date: {$lte: ?2} }, { $or: [ { 'title': {$regex: ?0, $options: 'i'} }, { 'body': {$regex: ?0, $options: 'i'} }, { 'comments.text': {$regex: ?0, $options: 'i'} } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
